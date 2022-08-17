c6a9ee7c2b40b253a63a37e4b3da715bad154fe6
le-mich <dev@le-mi.ch>
2022-08-17 22:03:41 +0200

    wal theming and new Makefile format

---
+++ dk/colors-dk.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+dkcmd set border colour \
+    focus='{color9}' \
+    unfocus='{color8}' \
+    urgent='{color12}' \
+    outer_focus='{color0}' \
+    outer_unfocus='{color0}' \
+    outer_urgent='{color13}' \
