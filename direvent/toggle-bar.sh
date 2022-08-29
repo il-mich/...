4973610d696509d8a3d968291af3c73101069654
le-mich <dev@le-mi.ch>
2022-08-29 23:29:40 +0200

    better handling of the padded bar

--- direvent/toggle-bar.sh
+++ direvent/toggle-bar.sh
@@ -5,14 +5,25 @@ out="$(dkcmd status type=full num=1)"
 for mon in $(echo "$out" | grep 'panels: ' | sed 's/panels: //' | sed 's/0x.*://')
 do
 	ws="$(echo "$out" | grep 'monitors: ' | sed "s/.*$mon:\([0-9]\+\).*/\1/")"
-	smartgap="$(echo "$out" | sed -n '/^workspaces:/,/^$/p' | grep -E "$ws:.* 0x" | cut -d" " -f8)"
+
+	workspaces="$(echo "$out" | sed -n '/^workspaces:/,/^$/p')"
+
+	inactive="$(echo "$workspaces" | grep -E "$ws:.* 0x00000000")"
+	padded="$(echo "$workspaces" | grep -E "$ws:.* 0 0 0 0")"
+	float="$(echo "$workspaces" | head -1 | grep -P "\*$ws:[^\s]+?:(none)")"
+	smartgap="$(echo "$workspaces" | grep -E "$ws:.* 0x" | cut -d" " -f8)"
+
+	#echo "i:$inactive"
+	#echo "p:$padded"
+	#echo "f:$float"
+	#echo "s:$smartgap"
 
 	bar="$(pgrep -nfx "polybar full")"
-	if [[ "$smartgap" == "0" ]]
+	if [[ "$inactive" == "" && "$padded" != "" && "$float" == "" && "$smartgap" == "1" ]]
 	then
-		polybar-msg -p $bar cmd hide
-	else
 		polybar-msg -p $bar cmd show
+	else
+		polybar-msg -p $bar cmd hide
 	fi
 done
 
